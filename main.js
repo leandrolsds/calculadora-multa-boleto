ab3779cb8c694c4063c13e19ee775fdb68871a9c
Leandro Santos <leandrosantosdesa@outlook.com>
2023-02-18 15:37:31 -0300

    atualização no texto do valor original

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ btn.addEventListener("click", (e) => {
     const valor_mora = document.getElementById("valor_mora").value;
     let valor_devido = valor_divida
     let data_recebida = new Date(data_vencimento)
-    let dias_atraso_recebidos = data_recebida.getDate()
     let data_atual = new Date();
     let diferenca_datas = Math.abs(data_atual - data_recebida);
     let calculo_dias = 1000 * 60 * 60 * 24; //milisegundos * segundos * minutos * horas dia
@@ -17,12 +16,12 @@ btn.addEventListener("click", (e) => {
     let calculo_multa = ((valor_multa * valor_divida) / 100).toFixed(2);
     let calculo_mora = (((valor_mora * valor_divida /100)/30)*quant_dias).toFixed(2);
     let totalAPagar = (Number(valor_divida) + Number(calculo_multa) + Number(calculo_mora)).toFixed(2);
-    
+ 
     if(valor_devido > 0) {
         if(quant_dias > 0) {
             if(valor_multa > 0 || valor_mora > 0) {
             let valor_original = document.getElementById("valor_original");
-            valor_original.innerHTML = "Valor Original da divida: "+valor_devido;
+            valor_original.innerHTML = "Valor Original da divida: R$ "+valor_devido;
             let dias_atraso = document.getElementById("dias_atraso");
             dias_atraso.innerHTML = "Dias em atraso: "+quant_dias;
                 let juros_multa = document.getElementById("juros_multa");
